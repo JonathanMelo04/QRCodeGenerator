d284f793558a9c386a0b2832f90f3b6108256b95
Jonathan Melo <jonathanmelo04@gmail.com>
2022-03-19 00:28:08 -0300

    Add logic that generates QRCode

--- app/src/main/java/com/example/qrcodegenerator/MainActivity.kt
+++ app/src/main/java/com/example/qrcodegenerator/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.qrcodegenerator
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.qrcodegenerator.databinding.ActivityMainBinding
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGenerateQRCode.setOnClickListener {
+            val text = binding.etData.text.toString().trim()
+
+            if (text.isNotBlank()) {
+                val bitmap = generateQRCode(text)
+                binding.ivQRCode.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+    private fun generateQRCode(text: String): Bitmap {
+        val width = 500
+        val height = 500
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val codeWriter = MultiFormatWriter()
+        try {
+            val bitMatrix = codeWriter.encode(text, BarcodeFormat.QR_CODE, width, height)
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+                }
+            }
+        } catch (e: WriterException) {
+            Log.d(TAG, "generateQRCode: ${e.message}")
+        }
+        return bitmap
     }
 }
